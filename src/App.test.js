cd675909e6ee6a5be3b2bdc03e71e0664384c2c7
Austin O <guitarguy199@gmail.com>
2023-08-11 11:23:08 -0600

    complete project

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import FeedbackForm from "./FeedbackForm";
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 
 describe("Feedback Form", () => {
   test("User is able to submit the form if the score is lower than 5 and additional feedback is provided", () => {
@@ -9,11 +10,20 @@ describe("Feedback Form", () => {
     render(<FeedbackForm onSubmit={handleSubmit} />);
 
     // You have to write the rest of the test below to make the assertion pass
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: score } });
+
+    const commentInput = screen.getByLabelText(/Comments:/);
+    fireEvent.change(commentInput, { target: { value: comment } });
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
 
     expect(handleSubmit).toHaveBeenCalledWith({
       score,
       comment,
     });
+    // expect(submitButton).toHaveAttribute(!isDisabled);
   });
 
   test("User is able to submit the form if the score is higher than 5, without additional feedback", () => {
@@ -22,6 +32,11 @@ describe("Feedback Form", () => {
     render(<FeedbackForm onSubmit={handleSubmit} />);
 
     // You have to write the rest of the test below to make the assertion pass
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, { target: { value: score } });
+
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
 
     expect(handleSubmit).toHaveBeenCalledWith({
       score,
